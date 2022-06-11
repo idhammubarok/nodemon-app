5a1b4bb6be07bea43a8d39e357d9094ec182e8dd
mubarok <idhammubarok43@gmail.com>
2022-06-11 07:42:05 +0700

    update name

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get("/", (req, res) => {
     res.send("Helloword!");
 });
 
+app.get("/name", (req, res) => {
+
+    res.send("My name is Idham Mubarok");
+});
+
 app.listen(PORT, () => {
     console.log(`Listening on port:${PORT}`);
 })
